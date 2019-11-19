38b72e74f21fe3ccb589de3d9eff04f068862345
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-11-19 09:08:14 +0530

    koushik commit 20191119 morning

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  API_URL: 'https://o820cv2lu8.execute-api.us-east-2.amazonaws.com/production/api',
+  API_URL: 'https://9ozbyvv5v0.execute-api.us-east-1.amazonaws.com/production/api/',
+  base64encode: 'https://nexgen.influxiq.com/php/index.php?encode=encodefile',
+  uploadurl: 'http://nexgen.influxiq.com/php/index.php',
+  // nodesslurl : 'https://dev.api.influxhostserver.com/',
+  nodesslurl : 'https://9ozbyvv5v0.execute-api.us-east-1.amazonaws.com/production/api/',
+  download_url : 'https://api.influxhostserver.com/download',
 };
