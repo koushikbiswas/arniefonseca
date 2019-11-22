c4c463c2540ced8c770603ff81ecaf1a8b3a4365
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-11-22 19:03:17 +0530

    koushik

--- src/app/component/backend/blogs/listing-blogcat/listing-blogcat.component.ts
+++ src/app/component/backend/blogs/listing-blogcat/listing-blogcat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-// import { CookieService } from 'ngx-cookie-service';
-// import { Router, ActivatedRoute } from '@angular/router';
-// import { ApiService } from '../../../../api.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../../api.service';
 
 @Component({
   selector: 'app-listing-blogcat',
@@ -11,45 +11,45 @@ import { Component, OnInit } from '@angular/core';
 export class ListingBlogcatComponent implements OnInit {
 
   //Listing for blog category
-  // public blogListConfig: any = {
-  //   apiBaseUrl:this.apiService.serverUrlDemo,
-  //   listEndPoint: "datalist",
-  //   datasource: "",
-  //   tableName: "blog_category",
-  //   updateurl: "addorupdatedata",
-  //   editUrl: "blog-category/edit/",
-  //   jwtToken: "this.cookieService.get('jwtToken')",
-  //   deleteEndPoint: "deletesingledata",
-  //   addLink: "blog-category/add",
-  //   view: "blog_category_view"
+  public blogListConfig: any = {
+    apiBaseUrl:this.apiService.serverUrlDemo,
+    listEndPoint: "datalist",
+    datasource: "",
+    tableName: "blog_category",
+    updateurl: "addorupdatedata",
+    editUrl: "blog-category/edit/",
+    jwtToken: "this.cookieService.get('jwtToken')",
+    deleteEndPoint: "deletesingledata",
+    addLink: "blog-category/add",
+    view: "blog_category_view"
 
-  // }
+  }
   
 
   // public blogCatList: any;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, private cookieService: CookieService, public apiService: ApiService) {
 
-    // private activatedRoute: ActivatedRoute, private cookieService: CookieService, public apiService: ApiService
     
-    // let data: any = {
-    //   source:"blog_category",
-    //   endpoint: "datalist"
-    // }
-    // this.apiService.getDatalist(data).subscribe((result: any)=>{
-    //   console.log(result.res);
-    //   this.blogListConfig.datasource = result.res;
-    //   this.blogListConfig.jwtToken = this.cookieService.get('jwtToken');
-    // });
+    
+    let data: any = {
+      source:"blog_category",
+      endpoint: "datalist"
+    }
+    this.apiService.getDatalist(data).subscribe((result: any)=>{
+      console.log(result.res);
+      this.blogListConfig.datasource = result.res;
+      this.blogListConfig.jwtToken = this.cookieService.get('jwtToken');
+    });
   
   }
 
   ngOnInit() {
    
-    // this.activatedRoute.data.forEach((data: any) => {
-    //   this.blogListConfig.datasource = data.blogCatList.res;
-    //    console.log('>>>>>koushikcat>>>>>>>>>',this.blogCatList)
-    // })
+    this.activatedRoute.data.forEach((data: any) => {
+      this.blogListConfig.datasource = data.blogCatList.res;
+      //  console.log('>>>>>koushikcat>>>>>>>>>',this.blogCatList)
+    })
    
     
   }
