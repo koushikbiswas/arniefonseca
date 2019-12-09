de5eb1c85f1d4c20fa298b79d6400952ac1ef42e
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-12-09 18:16:28 +0530

    koushik

--- src/app/component/frontend/blogdetail/blogdetail.component.ts
+++ src/app/component/frontend/blogdetail/blogdetail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Inject} from '@angular/core';
-import { MatAccordion,MatDialog,MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material';
+import { MatDialog,MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from '../../../api.service';
@@ -48,17 +48,19 @@ export class BlogdetailComponent implements OnInit {
   public blogcategory:any;
   public blogcategorycount:any;
   public blogcat:any;
-
+  public blogCount:any;
+  public resc: any;
+  public blogListing: any;
 
   
-  @ViewChild('myaccordion') myPanels: MatAccordion;
+  // @ViewChild('myaccordion') myPanels: MatAccordion;
 
-  openAll(){
-    this.myPanels.openAll();
-  }
-  closeAll(){
-    this.myPanels.closeAll();
-  }
+  // openAll(){
+  //   this.myPanels.openAll();
+  // }
+  // closeAll(){
+  //   this.myPanels.closeAll();
+  // }
 
   safeSrc: SafeResourceUrl;
 
@@ -129,10 +131,114 @@ export class BlogdetailComponent implements OnInit {
         
       })
  
+      
 
-       /**api service for blog_catagory total count by uttam */  
-      //  this.blogcategorycount = this.blogList.blogCatList.blog_category.length;
-       // console.log('>>>>>>>>>>>>>>>>>',this.blogcategorycount)
+      
+    // let data: any = {
+    //   source:"blog_category_view"
+    // }
+
+    // this.apiService.postDatawithoutToken("datalistwithouttoken", data).subscribe((result: any)=>{
+    //   // console.log(result.res);
+    //   this.blogcategorycount = result.resc;
+    //   // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogcategorycount);
+    // });
+
+
+
+    //    /**api service for blog_catagory total count by uttam */  
+    //   // this.blogcategorycount = this.blogList.blogCatList.blog_category.length;
+    //   // console.log('>>>>>>>>>>>>>>>>>',this.blogcategorycount)
+
+
+
+      
+
+    // // let data: any = {
+    // //   source:"blog_category_view"
+    // // }
+
+    // // this.apiService.postDatawithoutToken("datalistwithouttoken", data).subscribe((result: any)=>{
+    // //   // console.log(result.res);
+    // //   this.blogCategoryDataSource = result.res;
+    // //   // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogCategoryDataSource);
+    // // });
+
+
+    
+
+
+    
+    // let blogsdatacount: any = {
+    //   source:"blogs_view"
+    // }
+
+    // this.apiService.postDatawithoutToken("datalistwithouttoken", blogsdatacount).subscribe((result: any)=>{
+    //   // console.log(result.res);
+    //   this.blogCount = result.res;
+    //   // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogCount);
+    // });
+
+
+
+    // let blogscategory: any = {
+    //   source:"blog_category_view"
+    // }
+
+    // this.apiService.postDatawithoutToken("datalistwithouttoken", blogscategory).subscribe((result: any)=>{
+    //   // console.log(result.res);
+    //   this.blogCategory = result.res;
+    //   // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogCategory);
+    // });
+
+
+
+    //**all blog category and blog list from resolve in routing**//
+
+    this.activatedRoute.data.forEach((data: any) => {
+      this.blogList = data;
+      //  console.log('>>>>>>>>>>>>>>',this.blogList)
+
+    })
+
+  //****total blog list****//
+        let blogList: any = {
+          source:"blogs_view"
+        }
+        this.apiService.postDatawithoutToken("datalistwithouttoken", blogList).subscribe((result: any)=>{
+          // console.log(result.res);
+          this.blogCategoryDataSource = result.res;
+          // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogcategorycount);
+        });
+
+
+        // this.blogListing = this.blogList.blogCatList.blogs
+        // console.log('---------------',this.blogListing)
+      
+
+
+/**api service for total blog_catagory by uttam */
+
+      let blogCatData: any = {
+        source:"blog_category_view"
+      }
+      this.apiService.postDatawithoutToken("datalistwithouttoken", blogCatData).subscribe((result: any)=>{
+        // console.log(result.res);
+        this.blogCategoryDataSource = result.res;
+        // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogcategorycount);
+      });
+
+
+      /**api service for blog_catagory total count by uttam */  
+            let data: any = {
+            source:"blog_category_view"
+          }
+
+          this.apiService.postDatawithoutToken("datalistwithouttoken", data).subscribe((result: any)=>{
+            // console.log(result.res);
+            this.blogcategorycount = result.resc;
+            // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>', this.blogcategorycount);
+          });
        
   }
 
