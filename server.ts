12de76015f38311641ed4ef9550e1a745b418f05
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-02-05 13:06:44 +0530

    koushik pull first pull on 20200205

--- server.ts
+++ server.ts
@@ -1,24 +1,48 @@
 import 'zone.js/dist/zone-node';
-import {enableProdMode} from '@angular/core';
+// import {enableProdMode} from '@angular/core';
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+// import {ngExpressEngine} from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+// import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import {join} from 'path';
 
+
+
+const domino = require("domino");
+const fs = require("fs");
+const path = require("path");
+const templateA = fs
+  .readFileSync(path.join("dist/browser", "index.html"))
+  .toString();
+const win = domino.createWindow(templateA);
+// const win = domino.createWindow(template);
+win.Object = Object;
+global['Event'] = null;
+global["window"] = win;
+global["document"] = win.document;
+global['HTMLElement'] = win.HTMLElement;
+global['navigator'] = win.navigator;
+// import Buffer from Buffer()
+new Buffer('string') // Deprecated 
+Buffer.from('string') // New
+
+
 // Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+// enableProdMode();
 
 // Express server
 export const app = express();
+var compression = require('compression');
+
+app.use(compression());
 
 const PORT = process.env.PORT || 7100;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -44,3 +68,7 @@ app.get('*', (req, res) => {
   res.render('index', { req });
 });
 
+// Start up the Node server
+app.listen(PORT, () => {
+  // console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
